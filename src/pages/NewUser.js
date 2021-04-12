a771ae8009f4322b25631e49dfb75395fd878ed7
Jolanta Galczynska <jolanta.galczynska@gmail.com>
2021-04-12 17:40:50 +0200

    fixing styling on mobile version

--- src/pages/NewUser.js
+++ src/pages/NewUser.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import {  Grid, Paper, Avatar, TextField, FormControlLabel, Checkbox, Button } from '@material-ui/core';
 import PeopleIcon from '@material-ui/icons/People';
+import '../sass/NewUser.scss';
 import Footer from '../Footer';
 
 const NewUser = () => {
-    const newUser = {height: '96vh', paddingTop: '55px'}
-    const paperStyleForm = {padding: 20, height: '70vh', width: 400, margin: "48px auto"}
-    const avatarStyleForm = {backgroundColor: 'rgb(156, 45, 156)' }
-    const h1 = {paddingTop: 20, paddingBottom: 40}
-    const btnForm = { backgroundColor: 'rgb(156, 45, 156)', margin: '8px 0', marginTop: 20, marginBottom: 20 }
     return (
         <>
-        <Grid style={newUser}>
-            <Paper elevation={10} style={paperStyleForm}>
+        <Grid className="newUser">
+            <Paper elevation={10} className="styleForm">
                 <Grid align="center">
-                <Avatar style={avatarStyleForm}><PeopleIcon /></Avatar>
-                <h1 style={h1}>Ny bruker</h1>
+                <Avatar className="avatar"><PeopleIcon /></Avatar>
+                <h1>Ny bruker</h1>
                 </Grid>
                 <TextField label="Fullt navn" fullWidth required/>
                 <TextField label="E-mail" fullWidth required/>
@@ -30,7 +26,7 @@ const NewUser = () => {
                     }
                     label="Jeg aksepterer deres handlingsbetingelser"
                 />
-                   <Button style={btnForm} type="submit" color="primary"  variant="contained" fullWidth>Opprett bruker</Button>
+                   <Button className="btn" type="submit" color="primary"  variant="contained" fullWidth>Opprett bruker</Button>
             </Paper>
         </Grid>
     <Footer />
